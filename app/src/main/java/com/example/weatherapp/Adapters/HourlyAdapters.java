92cdaabbf3eb71cf49e824f8ab906677af145599
Bintang Satria Mulya <bintangsatriamulya88@gmail.com>
2024-12-29 00:40:14 +0800

    Create UI

--- app/src/main/java/com/example/weatherapp/Adapters/HourlyAdapters.java
+++ app/src/main/java/com/example/weatherapp/Adapters/HourlyAdapters.java
@@ -1,5 +1,38 @@
 package com.example.weatherapp.Adapters;
 
-public class HourlyAdapters {
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.weatherapp.Domains.Hourly;
+
+public class HourlyAdapters extends RecyclerView.Adapter<HourlyAdapters.viewHolder> {
+    @NonNull
+    @Override
+    public HourlyAdapters.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HourlyAdapters.viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder extends  RecyclerView.ViewHolder{
+        TextView hour,temp;
+        ImageView picPath;
+        public viewHolder(@NonNull ViewGroup parent) {
+            super(parent);
+        }
+
+    }
 
 }
